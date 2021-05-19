5a399ecbed9fcc246c7612355ee89dce8008c09c
Jon Greazel <jongreazel@pop-os.localdomain>
2021-05-18 21:16:26 -0500

    added react router

--- form-creator/src/App.tsx
+++ form-creator/src/App.tsx
@@ -1,14 +1,26 @@
 import React from "react";
 
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import { NewMeetForm, NewEventForm } from "./Forms";
+import Home from "./pages";
 
 import "./App.less";
 
 const App = () => (
   <div className="App">
-    <NewEventForm />
-    <hr />
-    <NewMeetForm />
+    <Router>
+      <Home>
+        <Switch>
+          <Route path="/track">
+            <NewEventForm />
+            <hr />
+            <NewMeetForm />
+          </Route>
+          <Route path="/"></Route>
+        </Switch>
+      </Home>
+    </Router>
   </div>
 );
 
---
+++ form-creator/src/pages/Wireframe.tsx
@@ -0,0 +1,32 @@
+import { Layout, Menu } from "antd";
+import { Link } from "react-router-dom";
+
+type Props = {
+  children: any;
+};
+
+const Wireframe = (props: Props) => {
+  const { Header, Content, Footer } = Layout;
+
+  return (
+    <Layout>
+      <Header>
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["1"]}>
+          <Menu.Item key="1">
+            <Link to="/">Home</Link>
+          </Menu.Item>
+
+          <Menu.Item key="2">
+            <Link to="/track">Track</Link>
+          </Menu.Item>
+        </Menu>
+      </Header>
+
+      <Content>{props.children}</Content>
+
+      <Footer>Buncha Junk</Footer>
+    </Layout>
+  );
+};
+
+export default Wireframe;
